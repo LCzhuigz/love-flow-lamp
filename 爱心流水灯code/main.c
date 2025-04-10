63c2bc58cb0c334e351ffeb41e02130b465b70a9
lczhuigz <lczhuigz@163.com>
2025-04-10 10:43:21 +0800

    加入两个新效果

--- 爱心流水灯code/main.c
+++ 爱心流水灯code/main.c
@@ -31,27 +31,27 @@ void main()
             clock_wise(i);
         }
  
-				for (i = 250; i > 0; i -= 50)
+		for (i = 250; i > 0; i -= 50)
         {
             anticlock_wise(i);
         }
-				Delay_ms(60);
-				clock_wise(20);
-				Delay_ms(60);
-				anticlock_wise(20);
+		Delay_ms(60);
+		clock_wise(20);
+		Delay_ms(60);
+		anticlock_wise(20);
         Delay_ms(60);
         two_flanks(10);
         Delay_ms(60);
         all_dance();
         Delay_ms(60);
         breath_effect();    // 新增呼吸效果
-				Delay_ms(60);
+		Delay_ms(60);
         cross_flash();      // 新增交叉闪烁
-				Delay_ms(60);
+		Delay_ms(60);
         random_flash();     // 新增随机闪烁
-				Delay_ms(60);
+		Delay_ms(60);
         heartbeat();        // 新增心跳效果
-				wave_effect();      // 新增波浪效果
+		wave_effect();      // 新增波浪效果
         Delay_ms(60);
         marquee();          // 新增跑马灯
         Delay_ms(60);
@@ -121,13 +121,13 @@ void anticlock_wise(uint x){
 }
 //两侧同时流水
 void two_flanks(uint x){
-		P1=0x7F;P3=0xFE;Delay_ms(x);P1=0xBF;P3=0xFD;Delay_ms(x);P1=0xDF;P3=0xFB;Delay_ms(x);P1=0xEF;P3=0xF7;Delay_ms(x);
-		P1=0xF7;P3=0xEF;Delay_ms(x);P1=0xFB;P3=0xDF;Delay_ms(x);P1=0xFD;P3=0xBF;Delay_ms(x);P1=0xFE;P3=0x7F;Delay_ms(x);
-		P1=0xFF;P3=0xFF;Delay_ms(x);
+	P1=0x7F;P3=0xFE;Delay_ms(x);P1=0xBF;P3=0xFD;Delay_ms(x);P1=0xDF;P3=0xFB;Delay_ms(x);P1=0xEF;P3=0xF7;Delay_ms(x);
+	P1=0xF7;P3=0xEF;Delay_ms(x);P1=0xFB;P3=0xDF;Delay_ms(x);P1=0xFD;P3=0xBF;Delay_ms(x);P1=0xFE;P3=0x7F;Delay_ms(x);
+	P1=0xFF;P3=0xFF;Delay_ms(x);
 
-		P0=0xFD;P2=0xFD;Delay_ms(x);P0=0xFE;P2=0xFE;Delay_ms(x);P0=0xFB;P2=0xFB;Delay_ms(x);P0=0xF7;P2=0xF7;Delay_ms(x);
-		P0=0xEF;P2=0xEF;Delay_ms(x);P0=0xDF;P2=0xDF;Delay_ms(x);P0=0xBF;P2=0xBF;Delay_ms(x);P0=0x7F;P2=0x7F;Delay_ms(x);
-		P0=0xFF;P2=0xFF;Delay_ms(x);
+    P0=0xFD;P2=0xFD;Delay_ms(x);P0=0xFE;P2=0xFE;Delay_ms(x);P0=0xFB;P2=0xFB;Delay_ms(x);P0=0xF7;P2=0xF7;Delay_ms(x);
+	P0=0xEF;P2=0xEF;Delay_ms(x);P0=0xDF;P2=0xDF;Delay_ms(x);P0=0xBF;P2=0xBF;Delay_ms(x);P0=0x7F;P2=0x7F;Delay_ms(x);
+	P0=0xFF;P2=0xFF;Delay_ms(x);
 }
 
 // 新增效果1：呼吸灯效果（所有LED渐变亮度）
@@ -147,11 +147,11 @@ void breath_effect(void) {
 void random_flash(void) {
 	uchar i;
     for(i=0; i<8; i++) {
-        P0 = ~(0x01 << (i%8)); 
-        P1 = ~(0x01 << ((i+2)%8));
-        P2 = ~(0x01 << ((i+4)%8));
-        P3 = ~(0x01 << ((i+6)%8));
-        Delay_ms(80);
+    P0 = ~(0x01 << (i%8)); 
+    P1 = ~(0x01 << ((i+2)%8));
+    P2 = ~(0x01 << ((i+4)%8));
+    P3 = ~(0x01 << ((i+6)%8));
+    Delay_ms(80);
     }
 }
 
